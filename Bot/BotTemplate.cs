e99c11b9c9f4d707933b27f56ae8279bacdcd152
Daniel Gherghetta <145233374+gherghett@users.noreply.github.com>
2025-06-26 22:24:49 +0200

    Update BotTemplate.cs

--- Bot/BotTemplate.cs
+++ Bot/BotTemplate.cs
@@ -5,12 +5,12 @@ public class BotTemplate : IPokerBot
 {
     private static readonly Random rng = new();
 
-    public string Name => "BotTemplate";
+    public string Name => "Cool Bot";
 
     // GetAction is called by the game runner, an you need to return what action the bot should take, based on the GameState
     public PokerAction GetAction(GameState state)
     {
-        bool random = rng.NextDouble() > 0.5d;
+        bool random = rng.NextDouble() > 0.3d;
         if (state.ToCall == 0)
         {
             if (random && state.MyStack >= state.MinRaise)
